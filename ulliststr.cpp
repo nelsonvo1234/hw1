82952c5f273ffd2a08eb07c52ab1298475213692
Nelson Vo <nelsonvo2006@gmail.com>
2025-01-31 15:34:29 -0800

    Hopefully part 2 works?

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -26,6 +26,103 @@ size_t ULListStr::size() const
 
 // WRITE YOUR CODE HERE
 
+void ULListStr::push_back(const std::string& val){
+  if(head_ == NULL){
+    head_ = new Item();
+    head_->val[9] = val;
+    head_->last = 10;
+    head_->first = 9;
+    tail_ = head_;
+    size_ = 1;
+  }
+  else{
+    if(tail_->last == ARRSIZE){
+      Item* newTail = new Item();
+      tail_->next = newTail;
+      newTail->prev = tail_;
+      tail_ = newTail;
+      tail_->val[0] = val;
+      tail_->last = 1;
+    }
+    else{
+      tail_->val[tail_->last] = val;
+      tail_->last++;
+    }
+    size_++;
+  }
+}
+
+void ULListStr::push_front(const std::string& val){
+  if(head_ == NULL){
+    head_ = new Item();
+    head_->val[0] = val;
+    head_->last = 1;
+    tail_ = head_;
+    size_ = 1;
+  }
+  else{
+      if(head_->first == 0){
+        Item* newHead = new Item();
+        newHead->next = head_;
+        head_->prev = newHead;
+        head_ = newHead;
+        head_->val[9] = val;
+        head_->last = 10;
+        head_->first = 9;
+    }
+    else{
+      head_->val[head_->first - 1] = val;
+      head_->first--;
+    }
+    size_++;
+  }
+}
+
+void ULListStr::pop_back(){
+  tail_->last++;
+  if(tail_->first == tail_->last){
+    tail_ = tail_->prev;
+    delete tail_->next;
+  }
+  size_--;
+}
+
+void ULListStr::pop_front(){
+  head_->first++;
+  if(head_->first == head_->last){
+    head_ = head_->next;
+    delete head_->prev;
+  }
+  size_--;
+}
+
+std::string const & ULListStr::back() const{
+  if(tail_ != NULL){
+    return tail_->val[tail_->last];
+  }
+  return NULL;
+}
+
+std::string const & ULListStr::front() const{
+    if(head_ != NULL){
+    return head_->val[head_->first];
+  }
+  return NULL;
+  
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const{
+  if(loc >= size_ || loc < 0){
+    return NULL;
+  }
+  Item* current = head_;
+  while(loc >= (current->last - current->first)){
+    loc -= (current->last - current->first);
+    current = current->next;
+  }
+  return &current->val[loc + current->first];
+}
+
 void ULListStr::set(size_t loc, const std::string& val)
 {
   std::string* ptr = getValAtLoc(loc);
