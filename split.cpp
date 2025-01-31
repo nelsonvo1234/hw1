7d737b392ba3fd25324cf8b2242f9468d8f148ba
Nelson Vo <nqvo@usc.edu>
2025-01-31 04:55:52 +0000

    Moving to VS Code, debugging

--- split.cpp
+++ split.cpp
@@ -11,13 +11,55 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
 
 /* Add a prototype for a helper function here if you need */
+Node* pop(Node*& in);
+void pushBack(Node* in, Node* to);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  if (in == NULL){
+    return;
+  }
+  Node* current = pop(in);
+  if(in->value % 2 == 0){
+    if(evens != NULL){
+      pushBack(current, evens);
+    }
+    else{
+      evens = current;
+      evens->next= NULL;
+    }
+  }
+  else{
+    if(odds != NULL){
+      pushBack(current, odds);
+    }
+    else{
+      evens = current;
+      evens->next= NULL;
+    }
+  }
+  split(in, odds, evens);
+
 }
 
 /* If you needed a helper function, write it here */
+
+Node* pop(Node*& in){
+  Node* node = in;
+  in = in->next;
+  return node;
+}
+
+void pushBack(Node* in, Node* to){
+  if(to->next == NULL){
+    to -> next = in;
+    in -> next = NULL;
+  }
+  else{
+    pushBack(in, to->next);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -1,7 +1,7 @@
 /*
 CSCI 104: Homework 1 Problem 1
 
-Use this file to test your split.cpp file.
+Use this file to test yor split.cpp file.
 
 Create linked lists and split them with your split() function.
 
@@ -9,9 +9,34 @@ You can compile this file like this:
 g++ split.cpp test_split.cpp -o test_split
 */
 
+
+#include <iostream>
 #include "split.h"
+#include <cstddef>
 
 int main(int argc, char* argv[])
 {
-
+  std::cout << "started" << std::endl;
+  Node* even = NULL;
+  Node* odd = NULL;
+  Node* in = new Node(0, NULL);
+  std::cout << "initialized" << std::endl;
+  for(int i = 1; i < 10; i++){
+    Node* next = new Node(i, in);
+    in = next;
+  }
+  std::cout << "filled in" << std::endl;
+  split(in, odd, even);
+  do{
+    std::cout << odd -> value << std::endl;
+    Node* temp = odd;
+    odd = odd -> next;
+    delete temp;
+  }while(odd->next != NULL);
+  do{
+    std::cout << even -> value << std::endl;
+    Node* temp = even;
+    even = even -> next;
+    delete temp;
+  }while(even->next != NULL);
 }
